9c01ee8b5290b575b102daa4b2cf25cf46af140f
Markus Harboe Olsen <oel@oelfam.com>
2021-02-04 10:50:02 +0000

    version 0.6.0

--- R/clinmon.R
+++ R/clinmon.R
@@ -1,8 +1,8 @@
 # ==== DOCUMENTATION ====
 
-#' Hemodynamic Indexes Calculated From Clinical Monitoring (clinmon)
+#' Hemodynamic Indices Calculated From Clinical Monitoring (clinmon)
 #'
-#' `clinmon()` uses a *continuous* recording and returns a dataframe with hemodynamic indexes for every period, epoch or block depending on the chosen output. Includes `COest`, `CPPopt`, `CVRi`, `Dx`, `Mx`, `PI`, `PRx`, `PWA`, `RI`, and `Sx` (see details).
+#' `clinmon()` uses a *continuous* recording and returns a dataframe with hemodynamic indices for every period, epoch or block depending on the chosen output. Calculates `COest`, `CPPopt`, `CVRi`, `Dx`, `Mx`, `PI`, `PRx`, `PWA`, `RI`, and `Sx` (see *Hemodynamic indices*).
 #'
 #' @name clinmon
 #'
@@ -13,36 +13,36 @@
 #' blockmin = 0.5, epochmin = 0.5,
 #' output = "period", fast = FALSE)
 #'
-#' @param df Raw continuous recording with all numeric data and first column has to be time in seconds. (`dataframe`)
+#' @param df Raw *continuous* recording with all numeric data and first column has to be time in seconds. (`dataframe`)
 #'
-#' @param variables Defining the type and order of the recorded variables as a list. Middle cerebral artery blood velocity (`'mcav'`) Arterial blood pressure (`'abp'`), cerebral perfusion pressure (`'cpp'`), intracranial pressure (`'icp'`), and heart rate (`'hr'`) is currently supported. (`list`)
+#' @param variables Defining the type and order of the recorded variables as a list. Middle cerebral artery blood velocity (`'mcav'`), Arterial blood pressure (`'abp'`), cerebral perfusion pressure (`'cpp'`), intracranial pressure (`'icp'`), and heart rate (`'hr'`) is currently supported. (`list`)
 #'
-#' @param trigger Trigger with two columns: (1) start and (2) end of period to be analyzed. Every row is a period for analysis. Default is `NULL`. (`dataframe`)
+#' @param trigger Trigger with two columns: first is start, and second is end of periods to be analyzed. Every row corresponds to a period. Default is `NULL`, which results in analysis of the full dataframe. (`dataframe`)
 #'
-#' @param deleter Deleter with two columns: (1) start and (2) end of period with artefacts which need to be deleted. Every row is a period with artefacts. Default is `NULL`. (`dataframe`)
+#' @param deleter Deleter with two columns: first is start and second is end of period with artefacts, which need to be deleted. Every row is a period with artefacts. Default is `NULL`. (`dataframe`)
 #'
 #' @param blocksize Length of a block, in seconds. Default is `3`. (`numeric`)
 #'
-#' @param epochsize Size of epochs, in number of blocks. Default is `20`. (`numeric`)
+#' @param epochsize Size of epochs in number of blocks. Default is `20`. (`numeric`)
 #'
-#' @param overlapping The number of block which should overlap when calculating correlation based indexes, and remain blank if overlapping calculations should not be utilized. Default is `FALSE`. (`numeric`)
+#' @param overlapping The number of block which should overlap when calculating correlation based indices, and remain blank if overlapping calculations should not be utilized. Default is `FALSE`. (`numeric`)
 #'
 #' @param freq Frequency of recorded data, in Hz. Default is `1000`. (`numeric`)
 #'
-#' @param blockmin Minimum measurements required to create a block in ratio. Default is `0.5` corresponding to 50%. (`numeric`)
+#' @param blockmin Minimum measurements required to create a block in ratio. Default is `0.5` corresponding to 50%. If the block holds less than the defined ratio the block will be omitted. (`numeric`)
 #'
-#' @param epochmin Minimum blocks required to create an epoch in ratio. Default is `0.5` corresponding to 50%. (`numeric`)
+#' @param epochmin Minimum number of blocks required to create an epoch in ratio. Default is `0.5` corresponding to 50%. If the epoch holds less than the defined ration the epoch will be omitted. (`numeric`)
 #'
-#' @param output Select what the rows should represent in the output. Correlation based indexes are not presented when selecting blocks for every row. Currently `'block'`, `'epoch'`, `'period'` or `'cppopt'` is supported. Default is `'period'`. (`string`)
+#' @param output Select what each row should represent in the output. Correlation based indices are not presented when selecting blocks for every row. Currently `'block'`, `'epoch'`, `'period'` or `'cppopt'` is supported. Default is `'period'`. (`string`)
 #'
-#' @param fast Select if you want the data to aggregated resulting in a faster, but perhaps more imprecise run, in Hz. Default is `FALSE.` (`numeric`)
+#' @param fast Select if you want the data to aggregated before analysis resulting in a faster, but perhaps more imprecise run, in Hz. Default is `FALSE.` (`numeric`)
 #'
 #' @details
 #'
-#' Using a *continuous* raw recording, this `clinmon()` calculates hemodynamic indexes for every period, epoch or block depending on the chosen output.
+#' Using a *continuous* raw recording, `clinmon()` calculates hemodynamic indices for every period, epoch or block depending on the chosen output.
 #'
 #' ```
-#' head(data)
+#' View(data)
 #' ```
 #' | `time` | `abp` | `mcav` |
 #' | --: | --: | --: |
@@ -52,7 +52,7 @@
 #' | `301.82` | `82` | `70` |
 #' | `301.83` | `81` | `69` |
 #'
-#' To calculate the indexes insert the data and select the relevant variables.
+#' To calculate the indices insert the data and select the relevant variables.
 #'
 #' ```
 #' clinmon(df=data, variables=c("abp","mcav"))
@@ -73,75 +73,75 @@
 #' | `*_mean`           | The mean value of each variable for the `period`, `epoch` or `block`. |
 #' | `*_min`            | The minimum value of each variable for the `period`, `epoch` or `block`. |
 #' | `*_max`            | The maximum value of each variable for the `period`, `epoch` or `block`. |
-#' | `*`                | The indexes in each column. |
+#' | `*`                | The indices in each column. |
 #'
-#' @section Hemodynamic indexes:
-#' ## Estimated cardiac output (`COest`)
+#' @section Hemodynamic indices:
+#' ## `COest` | Estimated cardiac output
 #' *Required variables:* `abp`, `hr`; *Required output:* `-`.
 #'
 #' Estimated cardiac output (`COest`) is calculated by utilizing the method described by Koenig et al. \[1]:
 #'    \deqn{COest = PP / (SBP+DBP) * HR}
 #' PP: Pulse pressure; SBP: systolic blood pressure; DBP: diastolic blood pressure; HR: heart rate.
 #'
-#' ## Optimal cerebral perfusion pressure (`CPPopt`)
-#' *Required variables:* `cpp`, `icp`; *Required output:* `period`.
+#' ## `CPPopt` | Optimal cerebral perfusion pressure
+#' *Required variables:* `abp`, `icp`; *Required output:* `period`.
 #'
 #' Optimal cerebral perfusion pressure (`CPPopt`) is calculated utilizing the method described by Steiner et al. \[2]. The CPPopt return `NA` if CPPopt is the maximum or minimum CPP investigated. CPPopt is recommended to only be calculated after 'several hours' of recording:
-#'    \deqn{CPPopt = 5 mmHg CPP interval with lowest mean PRx ) }
+#'    \deqn{CPPopt = 5 mmHg_CPP_interval_with_lowest_mean_PRx ) }
 #' CPP: cerebral perfusion pressure; PRx: Pressure reactivity index.
 #'
-#' ## Cardiovascular resistance index (`CVRi`)
+#' ## `CVRi` | Cardiovascular resistance index
 #' *Required variables:* `abp`, `mcav`; *Required output:* `-`.
 #'
 #' Cardiovascular resistance index (`CVRi`) is calculated utilizing the method described by Fan et al. \[3]:
 #'    \deqn{CVRi = mean ABP / mean MCAv }
 #' ABP: arterial blood pressure; MCAv: middle cerebral artery blood velocity.
 #'
-#' ## Diastolic flow index (`Dx`)
-#' *Required variables:* `cpp`, `abp`, `mcav`; *Required output:* `epoch`, `period`.
+#' ## `Dx` | Diastolic flow index
+#' *Required variables:* `cpp`/`abp`, `mcav`; *Required output:* `epoch`, `period`.
 #'
 #' Diastolic flow index (`Dx`) is calculated utilizing the method described by Reinhard et al. \[4]:
 #'    \deqn{Dx = cor( mean CPP / min MCAv ) }
 #'    \deqn{Dxa = cor( mean ABP / min MCAv ) }
 #' cor: correlation coefficient; CPP: cerebral perfusion pressure; ABP: arterial blood pressure; MCAv: middle cerebral artery blood velocity.
 #'
-#' ## Mean flow index (`Mx`)
-#' *Required variables:* `cpp`, `abp`, `mcav`; *Required output:* `epoch`, `period`.
+#' ## `Mx` | Mean flow index
+#' *Required variables:* `cpp`/`abp`, `mcav`; *Required output:* `epoch`, `period`.
 #'
 #' Mean flow index (`Mx`) is calculated utilizing the method described by Czosnyka et al. \[5]:
 #'    \deqn{Mx = cor( mean CPP / mean MCAv ) }
 #'    \deqn{Mxa = cor( mean ABP / mean MCAv ) }
 #' cor: correlation coefficient; CPP: cerebral perfusion pressure; ABP: arterial blood pressure; MCAv: middle cerebral artery blood velocity.
 #'
-#' ## Gosling index of pulsatility  (`PI`)
+#' ##  `PI` | Gosling index of pulsatility
 #' *Required variables:* `mcav`; *Required output:* `-`.
 #'
 #' Gosling index of pulsatility (`PI`) is calculated utilizing the method described by Michel et al. \[6]:
 #'    \deqn{PI = (systolic MCAv - diastolic MCAv) / mean MCAv  }
 #' MCAv: middle cerebral artery blood velocity.
 #'
-#' ## Pressure reactivity index (`PRx`)
-#' *Required variables:* `cpp`, `icp`; *Required output:* `epoch`, `period`.
+#' ## `PRx` | Pressure reactivity index
+#' *Required variables:* `abp`, `icp`; *Required output:* `epoch`, `period`.
 #'
 #' Pressure reactivity index (`PRx`) is calculated utilizing the method described by Czosnyka et al. \[7]:
-#'    \deqn{PRx = cor( mean CPP / mean ICP ) }
+#'    \deqn{PRx = cor( mean ABP / mean ICP ) }
 #' cor: correlation coefficient; CPP: cerebral perfusion pressure; ICP: intracranial pressure.
 #'
-#' ## Pulse wave amplitude (`PWA`)
-#' *Required variables:* `cpp`, `icp`, `abp`, `mcav`; *Required output:* `-`.
+#' ## `PWA` | Pulse wave amplitude
+#' *Required variables:* `cpp`/`icp`/`abp`/`mcav`; *Required output:* `-`.
 #'
 #' Pulse wave amplitude (`PWA`) is calculated utilizing the method described by Norager et al. \[8]:
 #'    \deqn{PWA = systolic - diastolic }
 #'
-#' ## Pourcelot’s resistive (resistance) index (`RI`)
+#' ## `RI` | Pourcelots resistive (resistance) index
 #' *Required variables:* `mcav`; *Required output:* `-`.
 #'
-#' Pourcelot’s resistive (resistance) index (`RI`) is calculated utilizing the method described by Forster et al. \[9]:
+#' Pourcelots resistive (resistance) index (`RI`) is calculated utilizing the method described by Forster et al. \[9]:
 #'    \deqn{RI = (systolic MCAv - diastolic MCAv) / systolic MCAv  }
 #' MCAv: middle cerebral artery blood velocity.
 #'
-#' ## Systolic flow index (`Sx`)
-#' *Required variables:* `cpp`, `abp`, `mcav`; *Required output:* `epoch`, `period`.
+#' ## `Sx` | Systolic flow index
+#' *Required variables:* `cpp`/`abp`, `mcav`; *Required output:* `epoch`, `period`.
 #'
 #' Systolic flow index (`Sx`) is calculated utilizing the method described by Czosnyka et al. \[5]:
 #'    \deqn{Sx = cor( mean CPP / systolic MCAv ) }
@@ -161,9 +161,9 @@
 #'
 #'
 #' @examples
-#' df <- data.frame(seq(1, 901, 0.01),
-#'          rnorm(90001), rnorm(90001))
-#' clinmon(df, variables=c("abp","mcav"), freq=50)
+#'
+#' data(testdata)
+#' clinmon(df.data10, variables=c('abp','mcav','hr'), freq=10)
 #'
 #' @export
 #
@@ -184,6 +184,10 @@ clinmon <- function(
    output = "period", fast = FALSE
 ){
    colnames(df) <- c("t",variables)
+   if(any(variables == "abp") & any(variables == "icp") & !any(variables == "cpp")){
+      df$cpp <- df$abp-df$icp
+      variables <- c(variables,"cpp")
+   }
 
    #OPTIMIZE
    df <- Z.fast(df,freq,fast)
